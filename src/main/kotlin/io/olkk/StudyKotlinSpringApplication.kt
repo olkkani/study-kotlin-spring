da9734d6334700066e461b68dd019e96e222af28
Chajin Ahn <infilljean@gmail.com>
2022-07-05 21:44:20 +0900

    project init

---
+++ src/main/kotlin/io/olkk/StudyKotlinSpringApplication.kt
@@ -0,0 +1,11 @@
+package io.olkk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyKotlinSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudyKotlinSpringApplication>(*args)
+}
---
+++ src/test/kotlin/io/olkk/StudyKotlinSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.olkk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudyKotlinSpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
