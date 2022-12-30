7b3e5bb01e5a0d3aaf86f948b33c46e96a5ec777
chanjin_ahn <olkkani@gmail.com>
2022-12-30 18:25:23 +0900

    :package: update main package name
    도메일 이름 변경

--- src/main/kotlin/io/olkkani/StudyKotlinSpringApplication.kt
+++ src/main/kotlin/io/olkkani/StudyKotlinSpringApplication.kt
@@ -1,4 +1,4 @@
-package io.olkk
+package io.olkkani
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- src/test/kotlin/io/olkkani/StudyKotlinSpringApplicationTests.kt
+++ src/test/kotlin/io/olkkani/StudyKotlinSpringApplicationTests.kt
@@ -1,4 +1,4 @@
-package io.olkk
+package io.olkkani
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
