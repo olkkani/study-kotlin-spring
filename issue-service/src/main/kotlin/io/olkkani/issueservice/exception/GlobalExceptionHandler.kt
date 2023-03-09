6c6576e986b90887138c2ab0d44d2e9684ab2624
chanjin_ahn <olkkani@gmail.com>
2023-03-09 18:01:23 +0900

    :sparkles: Add ErrorrResponse

---
+++ issue-service/src/main/kotlin/io/olkkani/issueservice/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,33 @@
+package io.olkkani.issueservice.exception
+
+import com.auth0.jwt.exceptions.TokenExpiredException
+import mu.KotlinLogging
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    private val logger = KotlinLogging.logger{}
+
+    @ExceptionHandler(ServerException::class)
+    fun handlerServerException(ex:ServerException): ErrorResponse {
+        logger.error { ex.message }
+
+        return ErrorResponse(code = ex.code, message = ex.message)
+    }
+
+    @ExceptionHandler(TokenExpiredException::class)
+    fun handlerServerException(ex:TokenExpiredException): ErrorResponse {
+        logger.error { ex.message }
+
+        return ErrorResponse(code = 401, message = "Token Expired Error")
+    }
+
+    @ExceptionHandler(TokenExpiredException::class)
+    fun handleException(ex:TokenExpiredException): ErrorResponse {
+        logger.error { ex.message }
+
+        return ErrorResponse(code = 500, message = "Internal Server Error")
+    }
+}
