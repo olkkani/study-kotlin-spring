e0868801ad1469796a49500c8e534bb67d3e4314
chanjin_ahn <olkkani@gmail.com>
2023-03-14 17:46:20 +0900

    :sparkles: Add delete issue

--- issue-service/src/main/kotlin/io/olkkani/issueservice/web/IssueController.kt
+++ issue-service/src/main/kotlin/io/olkkani/issueservice/web/IssueController.kt
@@ -4,6 +4,8 @@ import io.olkkani.issueservice.config.AuthUser
 import io.olkkani.issueservice.domain.enums.IssueStatus
 import io.olkkani.issueservice.model.IssueRequest
 import io.olkkani.issueservice.service.IssueService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -41,4 +44,12 @@ class IssueController (
         @PathVariable id: Long,
         @RequestBody request: IssueRequest,
     ) = issueService.edit(authUser.userId, id, request)
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete (
+        authUser: AuthUser,
+        @PathVariable id: Long,
+    ) {
+        issueService.delete(id)
+    }
 }
